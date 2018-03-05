c175971f13df6b65adc2a95e1c31eff54054db96
Evey92 <evey9207@gmail.com>
2018-03-05 15:56:32 -0600

    +Ya se genera grid, aun no tiene minas lel.

---
+++ MineSweeper/MineSweeper/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class Form1 : Form
+    {
+        Form1 NewForm = new Form1();
+        int gameWidth, gameHeight;
+        TextBox h = new TextBox();
+        TextBox w = new TextBox();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDynamicCompontent();
+        }
+        private void InitializeDynamicCompontent()
+        {
+            ///esto es de mientras
+            w.Location = new Point(this.Width / 3, 100);
+            h.Location = new Point(2 * this.Width / 3, 100);
+
+        }
+        
+        private void nuevoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowOptions();
+           
+            NewForm.Show();
+            this.Dispose(false);
+            GridManager grid = new GridManager();
+            grid.gridGenerator(gameWidth, gameHeight, this);
+            MessageBox.Show("GO!");
+
+        }
+        private void ShowOptions()
+        {
+            using (Options gameSize = new Options())
+            {
+                if (gameSize.ShowDialog() == DialogResult.OK)
+                {
+                    gameWidth = gameSize.sWidth;
+                    gameHeight = gameSize.sHeight;
+                    h.Text = gameHeight.ToString();
+                    w.Text = gameWidth.ToString();
+                }
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void acercaDeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("There's no help here Boi", "Ayuda",MessageBoxButtons.OK);
+        }
+
+        private void créditosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                ("Proyecto IDV Interfaces \n Segundo parcial\n \n \n -Alberto Villano \n -Marco Millan \n -Ivan Preciado"),
+                "Ayuda", MessageBoxButtons.OK);
+        }
+
+        private void buscaminasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("En serio quieres salir?", "Salir", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+            
+        }
+    }
+}
---
+++ MineSweeper/MineSweeper/Options.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MineSweeper
+{
+    public partial class Options : Form
+    {
+        public int sWidth, sHeight;
+        
+        TextBox tWidth = new TextBox();
+        TextBox tHeight = new TextBox();
+        public Options()
+        {
+
+            InitializeComponent();
+            InitializeDynamicComponent();
+        }
+
+        public void InitializeDynamicComponent()
+        {
+            this.Height = 200;
+            this.Width = 200;
+            Button 
+                wUp = new Button(),
+                hUp = new Button(),
+                wDown = new Button(),
+                hDown = new Button(),
+                EButton = new Button(),
+                startButton = new Button();
+            Label
+                lWidth = new Label(),
+                lHeight = new Label(),
+                title = new Label();
+            startButton.DialogResult = DialogResult.OK;
+            startButton.Tag = DialogResult.OK;
+            startButton.Text = DialogResult.OK.ToString();
+            EButton.Text = DialogResult.OK.ToString();
+            title.Text = "Selecciona el tamaño del juego";
+            title.Width = TextRenderer.MeasureText(title.Text, title.Font).Width;
+            wDown.Text = "<";
+            wDown.Width = TextRenderer.MeasureText(wDown.Text, wDown.Font).Width*2;
+            wUp.Text = ">";
+            wUp.Width = TextRenderer.MeasureText(wUp.Text, wUp.Font).Width*2;
+            hUp.Text = ">";
+            hUp.Width = TextRenderer.MeasureText(hUp.Text, hUp.Font).Width * 2;
+            hDown.Text = "<";
+            hDown.Width = TextRenderer.MeasureText(hDown.Text, hDown.Font).Width * 2;
+            lWidth.Text = "Ancho";
+            lWidth.Size = TextRenderer.MeasureText(lWidth.Text, lWidth.Font);
+            lHeight.Text = "Alto";
+            lHeight.Size = TextRenderer.MeasureText(lWidth.Text, lWidth.Font);
+            tWidth.Text = "5";
+            tWidth.Width = 30;
+            tHeight.Text = "5";
+            tHeight.Width = 30;
+            tWidth.Height = wDown.Size.Height;
+            tHeight.Height = wDown.Size.Height;
+            title.Location = new Point
+                (((this.Width/2)-(title.Width/2)),10);
+            lWidth.Location = new Point
+               (((this.Width / 5) - (lWidth.Size.Width / 2)), ((this.Height / 3) - (lWidth.Height / 2)));
+            wDown.Location = new Point
+                (((lWidth.Location.X) +(lWidth.Size.Width)+1), (lWidth.Location.Y));
+            tWidth.Location = new Point
+                (((wDown.Location.X) + (wDown.Size.Width) + 1), (lWidth.Location.Y+1));
+            wUp.Location = new Point
+                (((tWidth.Location.X) + (tWidth.Size.Width) + 1), (lWidth.Location.Y));
+            lHeight.Location = new Point
+                (((this.Width / 5) - (lHeight.Size.Width / 2)), ((tWidth.Location.Y) + (tWidth.Size.Height) + 5));
+            hDown.Location = new Point
+                (((lHeight.Location.X) + (lHeight.Size.Width) + 1), (lHeight.Location.Y));
+            tHeight.Location = new Point
+                (((hDown.Location.X) + (hDown.Size.Width) + 1), (lHeight.Location.Y + 1));
+            hUp.Location = new Point
+                (((tHeight.Location.X) + (tHeight.Size.Width) + 1), (tHeight.Location.Y));
+            EButton.Location = new Point
+                (((this.Width / 2) - (EButton.Width / 2)), ((2*this.Height / 3) - (EButton.Height / 2)));
+            this.SizeChanged += new EventHandler(WindowResize);
+            EButton.Click+= new EventHandler(EButton_Click);
+            startButton.Click += new EventHandler(OptionsButton_Click);
+            wUp.Click += new EventHandler(wUp_Click);
+            hUp.Click += new EventHandler(hUp_Click);
+            wDown.Click += new EventHandler(wDown_Click);
+            hDown.Click += new EventHandler(hDown_Click);
+            Controls.Add(title);
+            Controls.Add(lWidth);
+            Controls.Add(lHeight);
+            Controls.Add(EButton);
+            Controls.Add(tWidth);
+            Controls.Add(tHeight);
+            Controls.Add(hUp);
+            Controls.Add(hDown);
+            Controls.Add(wDown);
+            Controls.Add(wUp);
+
+        }
+        private void WindowResize(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dont touch that!","HEY!",MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void wUp_Click(object sender, EventArgs e)
+        {
+            int up = 5;
+            if (tWidth.TextLength!=0)
+            {
+                up = int.Parse(tWidth.Text);
+                if (up >= 100)
+                {
+                    up = 100;
+                }
+                if (up<=5)
+                {
+                    up = 5;
+                }
+                    
+            }
+            up++;
+            tWidth.Text = up.ToString();
+
+
+        }
+        private void hUp_Click(object sender, EventArgs e)
+        {
+            int up = 5;
+            if (tHeight.TextLength != 0)
+            {
+                up = int.Parse(tHeight.Text);
+                up++;
+                if (up >= 100)
+                {
+                    up = 100;
+                }
+                if (up <= 5)
+                {
+                    up = 5;
+                }
+                
+            }
+            tHeight.Text = up.ToString();
+        }
+        private void wDown_Click(object sender, EventArgs e)
+        {
+            int down = 5;
+            if (tWidth.TextLength != 0)
+            {
+                down = int.Parse(tWidth.Text);
+                down--;
+                if (down >= 100)
+                {
+                    down = 100;
+                }
+                if (down <= 5)
+                {
+                    down = 5;
+                }
+
+            }
+
+            tWidth.Text = down.ToString();
+        }
+        private void hDown_Click(object sender, EventArgs e)
+        {
+            int down = 5;
+            if (tHeight.TextLength != 0)
+            {
+                down = int.Parse(tHeight.Text);
+                down--;
+                if (down >= 100)
+                {
+                    down = 100;
+                }
+                if (down <= 5)
+                {
+                    down = 5;
+                }
+
+            }
+
+            tHeight.Text = down.ToString();
+        }
+        private void Options_Load(object sender, EventArgs e)
+        {
+
+        }
+        public void EButton_Click(object sender, EventArgs e)
+        {
+            if (
+                tHeight.TextLength == 0 || 
+                tWidth.TextLength == 0 || 
+                int.Parse(tHeight.Text) < 5||
+                int.Parse(tWidth.Text) < 5)
+            {
+                MessageBox.Show("Los valores no pueden ser menores a 5 o ser nulos", "Advertencia");
+            }
+            else
+            {
+               
+                sHeight = int.Parse(tHeight.Text);
+                sWidth = int.Parse(tWidth.Text);
+                OptionsButton_Click(sender, e);
+            }
+        }
+        public void OptionsButton_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            
+        }
+    }
+}
