a304453091fcda3de8afa825e363e8bbc0e89e44
Ivan <idv16c.ipreciado@uartesdigitales.edu.mx>
2018-03-09 13:44:16 -0600

    ed

--- MineSweeper/MineSweeper/GridManager.cs
+++ MineSweeper/MineSweeper/GridManager.cs
@@ -60,6 +60,7 @@ namespace MineSweeper
                     
                 }
             }
+            Cell defaultsize = new Cell();
             fileCount = yg;
             columnCount = xg;
             cellCount = xg * yg;
@@ -81,8 +82,6 @@ namespace MineSweeper
                 {
                     for (int i = 0; i < ListaMinas.Count(); i++)
                     {
-                        //if (mineCount < mineTotal)
-                        //{
                         if (ListaMinas[i] == ((Cell)x).id && ListaMinas[i] != firstCell)
                         {
                             if (ListaMinas[i] != firstCell)
@@ -92,7 +91,6 @@ namespace MineSweeper
                             mineCount++;
                             
                         }
-                    //}
                     }
                 }
             }
@@ -127,32 +125,6 @@ namespace MineSweeper
 
 
 
-        private int adjMines(int x, int y, int gridX, int gridY)
-        {
-            bool[,] mines = new bool[gridX, gridY];
-            bool hasMine = mines[x, y];
-            int mineCount = 0;
-            for (int nx = x - 1; nx <= x + 1; nx++)
-            {
-                if (nx < 0 || nx >= gridX)
-                    continue;  // Don't go out of bounds
-
-                for (int ny = y - 1; ny <= y + 1; ny++)
-                {
-                    if (ny < 0 || ny >= gridY)
-                        continue;  // Don't go out of bounds
-
-                    if (nx == x && ny == y)
-                        continue;  // Don't count the cell itself
-
-                    if (mines[nx, ny])
-                        mineCount += 1;
-                }
-            }
-            return mineCount;
-        }
-
-
         private void button1_MouseUp(object sender, MouseEventArgs e)
         {
             var btn = ((Cell)sender);
@@ -180,8 +152,10 @@ namespace MineSweeper
 
                             Face.BackgroundImage = Image.FromFile(@"Ded.png");
                             Face.BackgroundImageLayout = ImageLayout.Stretch;
+                            ((Form1)tableroporquemevalemadreelingles).StopTime();
+                            ((Form1)tableroporquemevalemadreelingles).timeLabel.Text = "00:00";
 
-                            MessageBox.Show("Perdiste alv.");
+                            MessageBox.Show("GAME OVER");
                             clearBoard();
                         }
                     }
@@ -197,9 +171,10 @@ namespace MineSweeper
                         CascadaMeValeVergaEstaEnEspanolFuckGringos();
                         gameStarted = true;
                         revealCells(btn);
-
-
+                        
                     }
+                    Face.BackgroundImage = Image.FromFile(@"Happy.png");
+                    Face.BackgroundImageLayout = ImageLayout.Stretch;
                     break;
 
                 case MouseButtons.Right:
@@ -250,41 +225,46 @@ namespace MineSweeper
 
         public void revealCells(Cell btn)
         {
-            if (btn.isMine && !btn.isRevealed)
+            if (!btn.isFlagged)
             {
-                btn.BackgroundImage = Image.FromFile(@"AtomicMine.png");
-                btn.BackgroundImageLayout = ImageLayout.Stretch;
-                btn.Text = "  ";
-                player.Play();
+                if (btn.isMine && !btn.isRevealed)
+                {
+                    btn.BackgroundImage = Image.FromFile(@"AtomicMine.png");
+                    btn.BackgroundImageLayout = ImageLayout.Stretch;
+                    btn.Text = "  ";
+                    player.Play();
 
-                //btn.BackColor = Color.Red;
+                    btn.BackColor = Color.Red;
+                    //btn.Enabled = false;
+                    gameLost = true;
+                    btn.isFlagged = false;
+                    btn.isRevealed = true;
 
-                gameLost = true;
-                btn.isFlagged = false;
-                btn.isRevealed = true;
+                }
+                if (!btn.isBlank && !btn.isMine)
+                {
+                    int adjacentMines = btn.adjMine;
+                    btn.Text = adjacentMines.ToString();
+                    btn.ForeColor = Color.Purple;
+                    btn.isFlagged = false;
+                    btn.isRevealed = true;
+                    Face.BackgroundImage = Image.FromFile(@"Happy.png");
+                    Face.BackgroundImageLayout = ImageLayout.Stretch;
+                    //btn.Enabled = false;
+                }
+                if (btn.isBlank)
+                {
+                    btn.Text = " ";
+                    btn.ForeColor = Color.Purple;
+                    btn.isFlagged = false;
+                    btn.isRevealed = true;
+                    Face.BackgroundImage = Image.FromFile(@"Happy.png");
+                    Face.BackgroundImageLayout = ImageLayout.Stretch;
+                    //btn.Enabled = false;
 
-            }
-            if(!btn.isBlank && !btn.isMine)
-            {
-                int adjacentMines = btn.adjMine;
-                btn.Text = adjacentMines.ToString();
-                btn.ForeColor = Color.Purple;
-                btn.isFlagged = false;
-                btn.isRevealed = true;
-                Face.BackgroundImage = Image.FromFile(@"Happy.png");
-                Face.BackgroundImageLayout = ImageLayout.Stretch;
-            }
-            if(btn.isBlank)
-            {
-                btn.Text = " ";
-                btn.ForeColor = Color.Purple;
-                btn.isFlagged = false;
-                btn.isRevealed = true;
-                Face.BackgroundImage = Image.FromFile(@"Happy.png");
-                Face.BackgroundImageLayout = ImageLayout.Stretch;
+                    cascade(btn);
 
-                cascade(btn);
-
+                }
             }
         }
 
@@ -301,6 +281,8 @@ namespace MineSweeper
                     }
                 }
             }
+            tableroporquemevalemadreelingles.AutoSize = true;
+            tableroporquemevalemadreelingles.AutoSizeMode = AutoSizeMode.GrowAndShrink;
         }
 
 
@@ -418,6 +400,10 @@ namespace MineSweeper
                                 ((Cell)x).isRevealed = true;
                                 cascade(((Cell)x));
                             }
+                            else if(!((Cell)x).isMine)
+                            {
+
+                            }
                         }
                         if ((btn.id - columnCount) == ((Cell)x).id)
                         {
@@ -503,63 +489,5 @@ namespace MineSweeper
                 }
             }
         }
-
-        //public void FirstMove(int x, int y, Random rand)
-        //{
-        //    //For any board, take the user's first revealed panel + any neighbors of that panel to X depth, and mark them as unavailable for mine placement.
-        //    var depth = 0.125 * Width; //12.5% (1/8th) of the board width becomes the depth of unavailable panels
-        //    var neighbors = GetNeighbors(x, y, (int)depth); //Get all neighbors to specified depth
-        //    neighbors.Add(GetPanel(x, y)); //Don't place a mine in the user's first move!
-
-        //    //Select random panels from set of panels which are not excluded by the first-move rule
-        //    var mineList = Panels.Except(neighbors).OrderBy(user => rand.Next());
-        //    var mineSlots = mineList.Take(MineCount).ToList().Select(z => new { z.X, z.Y });
-
-        //    //Place the mines
-        //    foreach (var mineCoord in mineSlots)
-        //    {
-        //        Panels.Single(panel => panel.X == mineCoord.X && panel.Y == mineCoord.Y).IsMine = true;
-        //    }
-
-        //    //For every panel which is not a mine, determine and save the adjacent mines.
-        //    foreach (var openPanel in Panels.Where(panel => !panel.IsMine))
-        //    {
-        //        var nearbyPanels = GetNeighbors(openPanel.X, openPanel.Y);
-        //        openPanel.AdjacentMines = nearbyPanels.Count(z => z.IsMine);
-        //    }
-        //}
-
-
-
-        //      var generateMines = function(grid, grid_x, grid_y, mine_count) {
-        //  var mine_value = -(mine_count * 2), mine_x, mine_y;
-        //      var m, n;
-
-        //  for (var k=0; k<mine_count; k++) {
-        //    while (true) {
-        //      mine_x = Math.floor(Math.random() * grid_x);
-        //      mine_y = Math.floor(Math.random() * grid_y);
-
-        //      // TODO : add more randomness and strategies here
-
-        //      if (0 <= grid[mine_x][mine_y]) {
-        //        break;
-        //      }
-        //    }
-        //    for (n=-1; n<2; n++) {
-        //      for (m=-1; m<2; m++) {
-        //        if (0 == n && 0 == m) {
-        //          grid[mine_x][mine_y] = mine_value;
-        //        } else if (_between(mine_x+n,0,grid_x-1) && _between(mine_y+m,0, grid_y-1)) {
-        //          grid[mine_x + n][mine_y + m]++;
-        //        }
-        //      }
-        //    }
-        //  }
-
-        //  return grid;
-        //};
-
-
     }
 }
